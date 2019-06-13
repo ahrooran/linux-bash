6e920d3be541f3fe9dbad84e0812dccb7cb07e78
aaru <ahrooran@hotmail.co.uk>
2019-06-13 12:35:43 +0100

    auto 2019_06_13__12_35_36

--- gitpush.sh
+++ gitpush.sh
@@ -1,9 +1,11 @@
+today=`date '+%Y_%m_%d__%H_%M_%S'`;
+
 git add .
 
 echo 'Enter the commit message:'
 read commitMessage
 
-git commit -m "$commitMessage"
+git commit -m "$commitMessage $today"
 
 echo 'Enter the name of the branch:'
 read branch
